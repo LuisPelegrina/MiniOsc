0986219d6d39ef26de5d2a52a8f25f72342f3478
LuisPelegrina <lpelegri@ugr.es>
2024-06-14 11:01:41 -0500

    start MiniOscProyect

---
+++ save_functions.py
@@ -0,0 +1,29 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import multiprocessing 
+
+def plot(file_name,q):
+    if q.empty():
+        print("Queue is empty!") 
+    else:          
+        print("Plotting")
+        data = q.get()
+        plt.plot(np.fromiter(data, dtype = float))
+        plt.savefig(file_name)
+
+def save(file_name, q):
+    if q.empty():
+        print("Queue is empty!") 
+    else:          
+        print("Saving data")
+        data = q.get()
+        time_pre_get = time.time()
+        with open(file_name, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Sample'] + ['Channel 1 [V]'])
+            for i, value in enumerate(data):
+                csvwriter.writerow([i, value])
+            time_post_get = time.time()
+            print("Writing time:",time_pre_get-time_post_get)
