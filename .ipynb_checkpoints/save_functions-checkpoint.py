4ad9f873718d520533599ff4d5aa2ec74ae495db
LuisPelegrina <lpelegri@ugr.es>
2024-06-20 15:56:53 -0500

    Add trigger configuration

--- .ipynb_checkpoints/save_functions-checkpoint.py
+++ .ipynb_checkpoints/save_functions-checkpoint.py
@@ -1,8 +1,14 @@
+from ctypes import *
+from dwfconstants import *
+import math
 import time
 import matplotlib.pyplot as plt
+import sys
 import numpy as np
 import csv
+import os
 import multiprocessing 
+import save_functions as sf
 
 def save_plot(file_name,q):
     if q.empty():
@@ -57,3 +63,40 @@ def save_multi(file_name, freq, q):
                 
             time_post_get = time.time()
             print("Writing time:",time_pre_get-time_post_get)
+
+def save_trigger(file_name, freq, sec, tick, ticksec ,q):
+    if q.empty():
+        print("Queue is empty!") 
+    else:          
+        data = q.get()
+        
+        s = time.localtime(sec.value)
+        ns = 1e9/ticksec.value*tick.value
+        ms = math.floor(ns/1e6)
+        ns -= ms*1e6
+        us = math.floor(ns/1e3)
+        ns -= us*1e3
+        ns = math.floor(ns)
+        print(time.strftime("%Y-%m-%d-%H-%M-%S", s) +"-"+str(ms).zfill(3)+"-"+str(us).zfill(3)+"-"+str(ns).zfill(3))
+        file_name = file_name + str(time.strftime("%Y-%m-%d-%H-%M-%S", s) +"-"+str(ms).zfill(3)+"-"+str(us).zfill(3)+"-"+str(ns).zfill(3)) + ".csv"
+    
+        with open(file_name, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Time (s)'] + ['Channel 1 [V]'] + ['Channel 2 [V]'] + ['Channel 3 [V]']+ ['Channel 4 [V]'])
+
+            if len(data[0]) != len(data[1]):
+                print("RANGE ERROR IN SAVING")
+                quit()
+            
+            if len(data[1]) != len(data[2]): 
+                print("RANGE ERROR IN SAVING")
+                quit()
+                
+            if len(data[1]) != len(data[3]): 
+                print("RANGE ERROR IN SAVING")
+                quit()
+                
+            for i in range(len(data[0])):
+                csvwriter.writerow([i*1./freq, data[0][i], data[1][i], data[2][i], data[3][i]])   
+              
+    
--- run_osc_trigger_configuration.py
+++ run_osc_trigger_configuration.py
@@ -1,19 +1,17 @@
-"""
-   DWF Python Example
-   Author:  Digilent, Inc.
-   Revision:  2022-03-08
 
-   Requires:                       
-       Python 2.7, 3
-"""
 
+#load things
 from ctypes import *
 from dwfconstants import *
 import math
 import time
-import sys
 import matplotlib.pyplot as plt
-import numpy
+import sys
+import numpy as np
+import csv
+import os
+import multiprocessing 
+import save_functions as sf
 
 
 if sys.platform.startswith("win"):
@@ -43,10 +41,26 @@ if hdwf.value == hdwfNone.value:
 
 dwf.FDwfDeviceAutoConfigureSet(hdwf, c_int(0)) # 0 = the device will only be configured when FDwf###Configure is called
 
+"""
+CONFIG FOR SUNSETS:
+2000 samples at 100e6 Hz rate
+
+SWB_CH: 200 mV total, ~100 mV offset at 7kV
+SWT_CH: 500 mV total, ~200 mV offset at 7kV
+CH4: Trigger: 750 mV total,  NIM(500 mV negative pulse signal), 0 V offset
+"""
+
+
+save_csv = True
+
 #To match sunset configuration we need 2k samples, to capture 20 mus window
-cSamples = 2000
+nSamples = 2000
 hzRate = 100e6
-rgdSamples = (c_double*cSamples)()
+hzAcq = c_double(hzRate)
+rgdSamples_channel1 = (c_double*nSamples)()
+rgdSamples_channel2 = (c_double*nSamples)()
+rgdSamples_channel3 = (c_double*nSamples)()
+rgdSamples_channel4 = (c_double*nSamples)()
 
 #set up acquisition
 dwf.FDwfAnalogInFrequencySet(hdwf, hzAcq)
@@ -55,23 +69,15 @@ dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(-1), c_int(1))
 dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(-1), c_double(5))
 dwf.FDwfAnalogInChannelFilterSet(hdwf, c_int(-1), c_int(3))
 
-
-"""
-CONFIG FOR SUNSETS:
-SWB_CH: 200 mV total, ~100 mV offset at 7kV
-SWT_CH: 500 mV total, ~200 mV offset at 7kV
-CH4: Trigger: 750 mV total,  NIM(500 mV negative pulse signal), 0 V offset
-"""
-
 #set up range channel by channel
 dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(0), c_double(0.2)) #1
-dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(1), c_double(0.2)) #2
-dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(2), c_double(0.4)) #3
-dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(3), c_double(0.4)) #4
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(1), c_double(5)) #2
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(2), c_double(5)) #3
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(3), c_double(1)) #4
 
 #set up offset channel by channel
-dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(0), c_double(-1)) #1
-dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(1), c_double(0.5)) #2
+dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(0), c_double(-0.1)) #1
+dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(1), c_double(0)) #2
 dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(2), c_double(0)) #3
 dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(3), c_double(0)) #4
 
@@ -80,56 +86,56 @@ dwf.FDwfAnalogInTriggerAutoTimeoutSet(hdwf, c_double(0)) #disable auto trigger
 dwf.FDwfAnalogInTriggerSourceSet(hdwf, trigsrcDetectorAnalogIn) #one of the analog in channels
 dwf.FDwfAnalogInTriggerTypeSet(hdwf, trigtypeEdge)
 dwf.FDwfAnalogInTriggerChannelSet(hdwf, c_int(3)) # Channel 4
-dwf.FDwfAnalogInTriggerLevelSet(hdwf, c_double(-100.0)) # -100.0mV
+dwf.FDwfAnalogInTriggerLevelSet(hdwf, c_double(-0.200)) # -200.0mV
 dwf.FDwfAnalogInTriggerConditionSet(hdwf, DwfTriggerSlopeFall) 
 
 # relative to middle of the buffer, with time base/2 T0 will be the first sample
-dwf.FDwfAnalogInTriggerPositionSet(hdwf, c_double(0.5*cSamples/hzRate)) 
+#dwf.FDwfAnalogInTriggerPositionSet(hdwf, c_double(0.5*nSamples/hzRate)) 
+dwf.FDwfAnalogInTriggerPositionSet(hdwf, c_double(0)) 
 
-dwf.FDwfAnalogInConfigure(hdwf, c_int(1), c_int(0))
 # wait at least 2 seconds with Analog Discovery for the offset to stabilize, before the first reading after device open or offset/range change
 time.sleep(2)
 
 print("Starting repeated acquisitions")
-dwf.FDwfAnalogInConfigure(hdwf, c_int(0), c_int(1))
+dwf.FDwfAnalogInConfigure(hdwf, c_int(1), c_int(1))
 
 
-saving_directory = "/daq/scratch/FC_mini_osc/triggered_data/"
+saving_directory = "Data/"
+csv_name = saving_directory + "trigger_"
+sec = c_uint(1)
+tick = c_uint(1)
+ticksec = c_uint(1)
 
-for iTrigger in range(10):
-    # new acquisition is started automatically after done state 
 
-    while True:
-        dwf.FDwfAnalogInStatus(hdwf, c_int(1), byref(sts))
-        if sts.value == DwfStateDone.value :
-            break
-        time.sleep(0.001)
+if __name__ == "__main__": 
+    # creating multiprocessing Queue 
+    q1 = multiprocessing.Queue(-1) 
     
-    dwf.FDwfAnalogInStatusData(hdwf, 0, rgdSamples, cSamples) # get channel 1 data
-    #dwf.FDwfAnalogInStatusData(hdwf, 1, rgdSamples, cSamples) # get channel 2 data
-    
-    dc = sum(rgdSamples)/len(rgdSamples)
-    print("Acquisition #"+str(iTrigger)+" average: "+str(dc)+"V")
-    
-    sec = c_uint()
-    tick = c_uint()
-    ticksec = c_uint()
-    
-    # acquisition software time for Analog Discovery and T0 with 8-10ns precision for ADP3X50
-    dwf.FDwfAnalogInStatusTime(hdwf, byref(sec), byref(tick), byref(ticksec))
-    s = time.localtime(sec.value)
-    ns = 1e9/ticksec.value*tick.value
-    ms = math.floor(ns/1e6)
-    ns -= ms*1e6
-    us = math.floor(ns/1e3)
-    ns -= us*1e3
-    ns = math.floor(ns)
-    print(time.strftime("%Y-%m-%d %H:%M:%S", s)+"."+str(ms).zfill(3)+"."+str(us).zfill(3)+"."+str(ns).zfill(3))
-    
-dwf.FDwfAnalogOutConfigure(hdwf, c_int(0), c_int(0))
-dwf.FDwfDeviceCloseAll()
+    try:
+        while True:
+            # Perform an acquisition and write the data to the CSV file
+            # creating new processes 
+            
+            p1 = multiprocessing.Process(target=sf.save_trigger, args=(csv_name, hzRate, sec, tick, ticksec ,q1,)) 
+            while True:
+                dwf.FDwfAnalogInStatus(hdwf, c_int(1), byref(sts))
+                if sts.value == DwfStateDone.value :
+                    break
+                time.sleep(0.001)
 
-plt.plot(numpy.fromiter(rgdSamples, dtype = numpy.float))
-plt.show()
+            dwf.FDwfAnalogInStatusData(hdwf, 0, rgdSamples_channel1, nSamples) # get channel 1 data
+            dwf.FDwfAnalogInStatusData(hdwf, 1, rgdSamples_channel2, nSamples) # get channel 2 data
+            dwf.FDwfAnalogInStatusData(hdwf, 2, rgdSamples_channel3, nSamples) # get channel 3 data
+            dwf.FDwfAnalogInStatusData(hdwf, 3, rgdSamples_channel4, nSamples) # get channel 4 data
 
+            dwf.FDwfAnalogInStatusTime(hdwf, byref(sec), byref(tick), byref(ticksec))   
+            py_data = [list(rgdSamples_channel1), list(rgdSamples_channel2), list(rgdSamples_channel3), list(rgdSamples_channel4)] 
+            q1.put(py_data)
+            p1.start()
+            
+
+    except KeyboardInterrupt:
+        print("Acquisition stopped by user")
+        dwf.FDwfAnalogOutReset(hdwf, c_int(0))
+        dwf.FDwfDeviceCloseAll()
 
