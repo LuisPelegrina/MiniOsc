6aa7a7c555224dac7c3b71d7ceb2e7f00669bf44
LuisPelegrina <lpelegri@ugr.es>
2024-06-26 13:39:57 -0500

    Start develop of Linux Mode

---
+++ Linux_mode/.ipynb_checkpoints/save_functions-checkpoint.py
@@ -0,0 +1,157 @@
+from ctypes import *
+from dwfconstants import *
+import math
+import time
+#import matplotlib.pyplot as plt
+import sys
+import numpy as np
+import csv
+import os
+import multiprocessing 
+#import configparser
+#import psycopg2
+#from datetime import datetime
+
+def save_multi(file_name, freq, q):
+    if q.empty():
+        print("Queue is empty!") 
+    else:          
+        print("Saving data")
+        data = q.get()
+        time_pre_get = time.time()
+        with open(file_name, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Time (s)'] + ['Channel 1 [V]'] + ['Channel 2 [V]'] + ['Channel 3 [V]']+ ['Channel 4 [V]'])
+
+            if len(data[0]) != len(data[1]):
+                print("RANGE ERROR IN SAVING")
+                quit()
+            
+            if len(data[1]) != len(data[2]): 
+                print("RANGE ERROR IN SAVING")
+                quit()
+               
+            if len(data[1]) != len(data[3]):
+                print("RANGE ERROR IN SAVING")
+                quit()
+            
+            for i in range(len(data[0])):
+                csvwriter.writerow([i*1./freq, data[0][i], data[1][i], data[2][i], data[3][i]])   
+                
+            time_post_get = time.time()
+            print("Writing time:",time_pre_get-time_post_get)
+            
+"""
+def read_db_config(filename, section):
+    parser = configparser.ConfigParser()
+    parser.read(filename)
+     
+    db_config = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db_config[param[0]] = param[1]
+    else:
+        raise Exception(f"Section {section} not found in the {filename} file")
+        
+    return db_config
+
+def generate_timestamps(start_time, freq, length):
+    increment = float(1/freq)
+    timestamps = [start_time]
+
+    for _ in range(1, length):
+        next_timestamp = timestamps[-1] + increment
+        timestamps.append(next_timestamp)
+
+    return timestamps
+
+def write_db(start_time, freq, down_spl, q):
+    if q.empty():
+        print("Queue is empty!")
+
+    else:
+        conn_params = read_db_config('/home/nfs/sbnddcs/configs/archiver/postgreSQL_db.ini', 'writer')
+        conn = psycopg2.connect(**conn_params)
+        cur = conn.cursor()
+        cur.execute("set search_path to dcs_prd;")
+        cur.execute("SELECT * FROM sample WHERE channel_id = 17492;")
+        data = q.get()
+        if len(data[0]) != len(data[1]):
+            print("RANGE ERROR IN SAVING")
+            quit()
+        if len(data[1]) != len(data[1]):
+            print("RANGE ERROR IN SAVING")
+            quit()
+        if len(data[2]) != len(data[1]):
+            print("RANGE ERROR IN SAVING")
+            quit()
+            
+        list_len = len(data[0])
+        timestamp_list = generate_timestamps(start_time, freq, list_len)
+
+        # Define the insert SQL query
+        insert_query = "INSERT INTO sample (channel_id,float_val,smpl_time,nanosecs,severity_id,status_id) VALUES (%s, %s, %s, %s, %s, %s);"
+        ch1_channel_id = 17492
+        ch2_channel_id = 17493
+        ch3_channel_id = 17494
+        ch4_channel_id = 17495
+
+        for i in range(0, len(data[0]), down_spl):
+            ch1_val = data[0][i]
+            ch2_val = data[1][i]
+            ch3_val = data[2][i]
+            #ch4_val = data[3][i]
+
+            timestamp = timestamp_list[i]
+            decimal_part = str(timestamp).split('.')[1]
+            dt_object = datetime.fromtimestamp(timestamp)
+            formatted_time = dt_object.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+            ch1_data = (ch1_channel_id, ch1_val, formatted_time, int(decimal_part), 5, 4);
+            ch2_data = (ch2_channel_id, ch2_val, formatted_time, int(decimal_part), 5, 4);
+            ch3_data = (ch3_channel_id, ch3_val, formatted_time, int(decimal_part), 5, 4);
+            cur.execute(insert_query, ch1_data)
+            cur.execute(insert_query, ch2_data)
+            cur.execute(insert_query, ch3_data)
+            conn.commit()
+        
+        cur.close()
+        conn.close()
+        
+def save_trigger(file_name, freq, sec, tick, ticksec ,q):
+    if q.empty():
+        print("Queue is empty!") 
+    else:          
+        data = q.get()
+        
+        s = time.localtime(sec.value)
+        ns = 1e9/ticksec.value*tick.value
+        ms = math.floor(ns/1e6)
+        ns -= ms*1e6
+        us = math.floor(ns/1e3)
+        ns -= us*1e3
+        ns = math.floor(ns)
+        print(time.strftime("%Y-%m-%d-%H-%M-%S", s) +"-"+str(ms).zfill(3)+"-"+str(us).zfill(3)+"-"+str(ns).zfill(3))
+        file_name = file_name + str(time.strftime("%Y-%m-%d-%H-%M-%S", s) +"-"+str(ms).zfill(3)+"-"+str(us).zfill(3)+"-"+str(ns).zfill(3)) + ".csv"
+    
+        with open(file_name, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Time (s)'] + ['Channel 1 [V]'] + ['Channel 2 [V]'] + ['Channel 3 [V]']+ ['Channel 4 [V]'])
+
+            if len(data[0]) != len(data[1]):
+                print("RANGE ERROR IN SAVING")
+                quit()
+            
+            if len(data[1]) != len(data[2]): 
+                print("RANGE ERROR IN SAVING")
+                quit()
+                
+            if len(data[1]) != len(data[3]): 
+                print("RANGE ERROR IN SAVING")
+                quit()
+                
+            for i in range(len(data[0])):
+                csvwriter.writerow([i*1./freq, data[0][i], data[1][i], data[2][i], data[3][i]])   
+"""
+    
---
+++ Linux_mode/run_osc_linux_mode_multi_channel.py
@@ -0,0 +1,150 @@
+"""
+   DWF Python Example
+   Author:  Digilent, Inc.
+   Revision:  2018-07-19
+
+   Requires:                       
+       Python 2.7, 3
+"""
+#load things
+from ctypes import *
+from dwfconstants import *
+import math
+import time
+import sys
+import csv
+import os
+import multiprocessing 
+import save_functions as sf
+
+if sys.platform.startswith("win"):
+    dwf = cdll.dwf
+elif sys.platform.startswith("darwin"):
+    dwf = cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf")
+else:
+    dwf = cdll.LoadLibrary("libdwf.so")
+
+#declare ctype variables
+hdwf = c_int()
+sts = c_byte()
+
+adq_frec = 100 # Hz
+down_spl = 10 # Save 1/down_spl of data to slow control DB
+hzAcq = c_double(adq_frec)
+record_time = 10 # sec
+
+save_csv = True
+write_db = False
+
+nSamples = int(record_time * adq_frec)
+rgdSamples_channel1 = (c_double*nSamples)()
+rgdSamples_channel2 = (c_double*nSamples)()
+rgdSamples_channel3 = (c_double*nSamples)()
+rgdSamples_channel4 = (c_double*nSamples)()
+
+if nSamples > 128000000.:
+    print("Number of samples exceed oscilloscope buffer, lower the time, frequency or use Record function")
+    quit()
+
+version = create_string_buffer(16)
+dwf.FDwfGetVersion(version)
+print("DWF Version: "+str(version.value))
+
+dwf.FDwfParamSet(DwfParamOnClose, c_int(0)) # 0 = run, 1 = stop, 2 = shutdown
+
+#print(DWF version
+version = create_string_buffer(16)
+dwf.FDwfGetVersion(version)
+print("DWF Version: "+str(version.value))
+
+
+#open device
+print("Opening first device")
+dwf.FDwfDeviceOpen(c_int(-1), byref(hdwf))
+
+
+if hdwf.value == hdwfNone.value:
+    szerr = create_string_buffer(512)
+    dwf.FDwfGetLastErrorMsg(szerr)
+    print(str(szerr.value))
+    print("failed to open device")
+    quit()
+
+   
+dwf.FDwfDeviceAutoConfigureSet(hdwf, c_int(0)) # 0 = the device will only be configured when FDwf###Configure is called
+
+#set up acquisition
+dwf.FDwfAnalogInFrequencySet(hdwf, hzAcq)
+dwf.FDwfAnalogInBufferSizeSet(hdwf, c_int(nSamples))
+dwf.FDwfAnalogInChannelEnableSet(hdwf, c_int(-1), c_int(1))
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(-1), c_double(5))
+dwf.FDwfAnalogInChannelFilterSet(hdwf, c_int(-1), c_int(3))
+
+#set up range channel by channel
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(0), c_double(0.2)) #1
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(1), c_double(0.2)) #2
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(2), c_double(0.4)) #3
+dwf.FDwfAnalogInChannelRangeSet(hdwf, c_int(3), c_double(0.4)) #4
+
+#set up offset channel by channel
+dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(0), c_double(-1)) #1
+dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(1), c_double(0.5)) #2
+dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(2), c_double(0)) #3
+dwf.FDwfAnalogInChannelOffsetSet(hdwf, c_int(3), c_double(0)) #4
+
+
+#wait at least 2 seconds for the offset to stabilize
+time.sleep(2)
+
+print("Starting repeated acquisitions")
+dwf.FDwfAnalogInConfigure(hdwf, c_int(1), c_int(1))
+
+saving_directory = "/daq/scratch/FC_mini_osc/monitoring_data/"
+
+if __name__ == "__main__": 
+    # creating multiprocessing Queue 
+    q1 = multiprocessing.Queue(-1) 
+    
+    try:
+        while True:
+            # Perform an acquisition and write the data to the CSV file
+            # Record the end time
+            start_time = time.time()
+            print(f"Starting acquisition {start_time}")
+            
+            csv_name = saving_directory + "oscilloscope_data_" + str(start_time).replace(".", "-" ) + "_ALL.csv"
+        
+            # creating new processes 
+            p1 = multiprocessing.Process(target=sf.save_multi, args=(csv_name, adq_frec, q1,)) 
+            
+            while True:
+                dwf.FDwfAnalogInStatus(hdwf, c_int(1), byref(sts))
+                if sts.value == DwfStateDone.value :
+                    break
+                time.sleep(0.001)
+
+            dwf.FDwfAnalogInStatusData(hdwf, 0, rgdSamples_channel1, nSamples) # get channel 1 data
+            dwf.FDwfAnalogInStatusData(hdwf, 1, rgdSamples_channel2, nSamples) # get channel 2 data
+            dwf.FDwfAnalogInStatusData(hdwf, 2, rgdSamples_channel3, nSamples) # get channel 3 data
+            dwf.FDwfAnalogInStatusData(hdwf, 3, rgdSamples_channel4, nSamples) # get channel 4 data
+
+            py_data = [list(rgdSamples_channel1), list(rgdSamples_channel2), list(rgdSamples_channel3), list(rgdSamples_channel4)]
+            if save_csv:
+                q1.put(py_data)
+                p1.start()
+            
+            end_time = time.time()
+            
+        
+            # Calculate the time difference
+            end_time = time.time()
+            print(f"Acquisition {start_time} completed")
+            print("End time:", end_time - start_time)
+
+
+    except KeyboardInterrupt:
+        print("Acquisition stopped by user")
+        dwf.FDwfAnalogOutReset(hdwf, c_int(0))
+        dwf.FDwfDeviceCloseAll()
+        
+
